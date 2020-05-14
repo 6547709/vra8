e279c1a7991ccd1f6f13407cab29869ba35be30d
GuoqiangLi <6547709@qq.com>
2020-05-14 21:21:52 +0800

    Update build-centos-vra8-template.sh

--- build-centos-vra8-template.sh
+++ build-centos-vra8-template.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-###Author: LiGuoqiang from VMware. ###
+###Autor: Li Guoqiang from VMware China. ###
+###install cloud-init. ### 
+yum install -y cloud-init
+###System Update###
+yum update -y
+
+###eanble root and password login for ssh. ###
+sudo sed -i 's/^disable_root: 1/disable_root: 0/g' /etc/cloud/cloud.cfg
+sudo sed -i 's/^ssh_pwauth:   0/ssh_pwauth:   1/g' /etc/cloud/cloud.cfg
+
+###disable vmware customization for cloud-init. ###
+sed -i 's/^disable_vmware_customization: false/disable_vmware_customization: true/g' /etc/cloud/cloud.cfg
+###setting datasouce is OVF only. ### 
+sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
+###disalbe clean tmp folder. ### 
+SOURCE_TEXT="v /tmp 1777 root root 10d"
+DEST_TEXT="#v /tmp 1777 root root 10d"
+sudo sed -i "s@${SOURCE_TEXT}@${DEST_TEXT}@g" /usr/lib/tmpfiles.d/tmp.conf
+sed -i "s/\(^.*10d.*$\)/#\1/" /usr/lib/tmpfiles.d/tmp.conf
+###Add After=dbus.service to vmtoolsd. ### 
+sed -i '/^After=vgauthd.service/a\After=dbus.service' /usr/lib/systemd/system/vmtoolsd.service
+
+###disable cloud-init in first boot,we use vmware tools exec customization. ### 
+touch /etc/cloud/cloud-init.disabled
+
 ###Create a runonce script for re-exec cloud-init. ###
 cat <<EOF > /etc/cloud/runonce.sh
 #!/bin/bash
@@ -77,26 +101,11 @@ cloud-init clean --logs --seed
 
 #Disabled Cloud-init
 touch /etc/cloud/cloud-init.disabled
-rm -rf /var/run/cron.reboot
-#cleanup shell history
-history -w
-history -c
-EOF
-###install cloud-init. ### 
-yum install -y cloud-init
-###System Update###
-yum update -y
-###disable vmware customization for cloud-init. ###
-sed -i 's/^disable_vmware_customization: false/disable_vmware_customization: true/g' /etc/cloud/cloud.cfg
-###setting datasouce is OVF only. ### 
-sed -i '/^disable_vmware_customization: true/a\datasource_list: [OVF]' /etc/cloud/cloud.cfg
-###disalbe clean tmp folder. ### 
-sed -i "s/\(^.*10d.*$\)/#\1/" /usr/lib/tmpfiles.d/tmp.conf
-###Add After=dbus.service to vmtoolsd. ### 
-sed -i '/^After=vgauthd.service/a\After=dbus.service' /usr/lib/systemd/system/vmtoolsd.service
 
-###disable cloud-init in first boot,we use vmware tools exec customization. ### 
-touch /etc/cloud/cloud-init.disabled
+#cleanup shell history
+echo > ~/.bash_history
+history -cw
+EOF
 ###change script execution permissions. ### 
 chmod +x /etc/cloud/runonce.sh /etc/cloud/clean.sh
 ###reload runonce.service. ### 
